24b6227dcfc04be6607afa079b8bd41a96d9c0d9
lmlLanuza <liammaenard.99@gmail.com>
2021-10-14 11:21:52 +0800

    Version 3 Update
    Added color to text. Fixed Variable names.

--- MapQuest_Enhanced.py
+++ MapQuest_Enhanced.py
@@ -1,69 +1,79 @@
-from tkinter import*
+import tkinter as tk 
 import urllib.parse
 import requests
 main_api = "https://www.mapquestapi.com/directions/v2/route?"
-key = "YXcZ6xIdAdBfJEijDlGasOPJoGrqf9GV"    # You should use your own key 
-root = Tk()
-root.geometry("1000x1000")
-root.title("Map Quest")
+key = "YXcZ6xIdAdBfJEijDlGasOPJoGrqf9GV"  
+root= tk.Tk() 
+root.geometry("1500x1500")
+ 
+canvas1 = tk.Canvas(root, width = 1000)
+canvas1.pack()
 
-heading = Label(text="Map Quest", bg ="black", fg="white", width="500", height="3")
-heading.pack()
+canvas2 = tk.Canvas(root, width = 1000)
+canvas2.pack()
 
-LabelLocation = Label(text="Enter your Current Location* ", font="Arial 15")
-location = Entry(root, width=50)
 
-LabelDestination = Label(text="Enter your Destination Location* ", font="Arial 15")
-destination = Entry(root, width=50)
-LabelLocation.pack()
-location.pack()
+location = tk.Label(root, text='Type your Location: ')
+canvas1.create_window(0, 50, window=location)
+entry1 = tk.Entry (root)
 
-LabelDestination.pack()
-destination.pack()
-def myClick():
+destination = tk.Label(root, text='Type your Destination: ')
+canvas1.create_window(0, 100, window=destination)
+entry2 = tk.Entry (root)
+canvas1.create_window(200, 50, window=entry1)
+canvas1.create_window(200, 100, window=entry2)
+
+def button():
+    global destination
+    global location
+    location = entry2.get()
+    destination = entry1.get()
+  # You should use your own key 
     while True:
-
         url = main_api + urllib.parse.urlencode({"key":key, "from":location, "to":destination})
         print ("URL ", (url))
         json_data = requests.get(url).json()
         json_status = json_data["info"]["statuscode"]
         if json_status == 0:
             argument = "\nAPI Status: " + str(json_status) + " = A successful route call.\n"\
-            "\n============================================="+ "\nDirections from " + (location.get()) + " to " + (destination.get())\
+            "\n============================================="+ "\nDirections from " + (location) + " to " + (destination)\
             +"\nTrip Duration: " + (json_data["route"]["formattedTime"])\
             +"\nKilometers: " + str("{:.2f}".format(json_data["route"]["distance"] * 1.6))\
             +"\nFuel Used (Ltr): " + str("{:.3f}".format(json_data["route"]["fuelUsed"]*3.78))\
-            + "\n============================================="
-            myLabel = Label(root, text=argument)
-            myLabel.pack()
+            + "\n=============================================\n"
+            Label1 = tk.Label(root, text= argument, bg='yellow')
+            canvas1.create_window(700, 100, window=Label1)
             for each in json_data["route"]["legs"][0]["maneuvers"]:
-                argument2 = (each["narrative"]) + " (" + str("{:.2f}".format((each["distance"])*1.61) + " km)")
-                myLabel2 = Label(root, text=argument2,font="Times 10",justify=LEFT)
-                myLabel2.pack()
+                argument2 = (each["narrative"]) + " \n(" + ("{:.2f}".format((each["distance"])*1.61) + " km)")
+                Label2 = tk.Label(root, text= argument2, bg='yellow')
+                canvas2.create_window(700,0,window=Label2)
+                
         elif json_status == 402:
             argument3 ="**********************************************"\
             +"Status Code: " + str(json_status) + "; Invalid user inputs for one or bothlocations."\
             +"**********************************************\n"
-            myLabel3 = Label(root, text=argument3)
-            mylabel3.pack()
+            Label3 = tk.Label(root, text= argument3, bg='yellow')
+            canvas1.create_window(500, 200, window=Label3)
+        
         elif json_status == 611:
             argument4 ="**********************************************"\
             + "Status Code: " + str(json_status) + "; Missing an entry for one or bothlocations."\
             +"**********************************************\n"
-            myLabel4 = Label(root, text=argument4)
-            mylabel4.pack()
+            Label4 = tk.Label(root, text= argument4, bg='yellow')
+            canvas2.create_window(500, 200, window=Label4)
+
         else:
             argument5 ="************************************************************************"\
             + "For Staus Code: " + str(json_status) + "; Refer to:"\
             + "https://developer.mapquest.com/documentation/directions-api/status-codes"\
             + "************************************************************************\n"   
-            myLabel5 = Label(root, text=argument5)
-            mylabel5.pack()
+            Label5 = tk.Label(root, text= argument5, bg='yellow')
+            canvas2.create_window(500, 200, window=Label5)
+     
         break
-       
-    
-myButton = Button(root, text="Submit", command=myClick)
-myButton.pack()
 
-  
+button1 = tk.Button (root, text='Find your Destination',command=button, bg='orange') # button to call the 'values' command above 
+canvas1.create_window(200,150, window=button1)
+ 
+
 root.mainloop()
